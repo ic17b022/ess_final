22824731fbf85be1fe7ca49eba18fe1e521f05cb
voltae <ic17b096@technikum-wien.at>
2018-12-27 17:55:48 +0100

    Created common.h for all shared headers, created oled_display.h and oled_display.c for the oled functionality

--- StartBIOS.c
+++ StartBIOS.c
@@ -1,34 +1,8 @@
 /*
  *  ======== StartBIOS.c ========
  */
-#include <stdbool.h>
-#include <stdint.h>
-#include <inc/hw_memmap.h>
 
-/* XDCtools Header files */
-#include <xdc/std.h>
-#include <xdc/cfg/global.h>
-#include <xdc/runtime/System.h>
-#include <xdc/runtime/Error.h>
-#include <xdc/runtime/Memory.h>
-
-/* BIOS Header files */
-#include <ti/sysbios/BIOS.h>
-#include <ti/sysbios/knl/Task.h>
-
-/* Instrumentation headers */
-#include <ti/uia/runtime/LogSnapshot.h>
-
-/* Driverlib headers */
-#include <driverlib/gpio.h>
-
-/* Board Header files */
-#include <Board.h>
-#include <EK_TM4C1294XL.h>
-
-/* Application headers */
-
-
+#include "local_inc/common.h"
 
 int main(void)
 {
